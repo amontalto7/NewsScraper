29dc9052be0173863bc09f978d85d40b085f9365
ant4u <anthonyimontalto@gmail.com>
2019-02-21 21:43:19 -0500

    article headlines displaying on page

--- public/assets/js/news.js
+++ public/assets/js/news.js
@@ -1,3 +1,20 @@
+// Grab the articles as a json
+$.getJSON("/articles", function(data) {
+  // For each one
+  for (var i = 0; i < data.length; i++) {
+    // Display the apropos information on the page
+    $("#articles").append(
+      "<p data-id='" +
+        data[i]._id +
+        "'>" +
+        data[i].headline +
+        "<br />" +
+        data[i].url +
+        "</p>"
+    );
+  }
+});
+
 $(document).on("click", ".btnScrape", function() {
   event.preventDefault();
   let section = $("#section")
