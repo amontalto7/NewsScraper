b9e603e31e17e60eba45220529d703aedfe8c3de
ant4u <anthonyimontalto@gmail.com>
2019-02-24 12:12:25 -0500

    moved functions outside of document.ready

--- public/assets/js/news.js
+++ public/assets/js/news.js
@@ -1,3 +1,52 @@
+// Function to handle article scraping
+function scrapeArticles() {
+  event.preventDefault();
+  var section = $("#section")
+    .val()
+    .trim();
+
+  console.log($);
+
+  $.ajax({
+    method: "GET",
+    url: "/scrape/" + section
+  }).then(function(data) {
+    console.log(data);
+  });
+}
+
+// Function to save articles
+function handleArticleSave() {
+  // This function is triggered when the user wants to save an article
+  // When we rendered the article initially, we attached a javascript object containing the headline id
+  // to the element using the .data method. Here we retrieve that.
+  event.preventDefault();
+  var articleToSave = $(this)
+    .parents(".card")
+    .data();
+
+  // Remove card from page
+  $(this)
+    .parents(".card")
+    .remove();
+
+  articleToSave.saved = true;
+  // Using a patch method to be semantic since this is an update to an existing record in our collection
+  $.ajax({
+    method: "PUT",
+    url: "/api/articles/" + articleToSave._id,
+    data: articleToSave
+  }).then(function(data) {
+    // If the data was saved successfully
+    if (data.saved) {
+      // Run the initPage function again. This will reload the entire list of articles
+      // initPage();
+      console.log("article saved");
+    }
+  });
+}
+
+// -------------------------------------- MAIN LOGIC ------------------------------- //
 $(document).ready(function() {
   $(document).on("click", ".btn.save", handleArticleSave);
   $(document).on("click", ".btnScrape", scrapeArticles);
@@ -5,52 +54,7 @@ $(document).ready(function() {
 
   // $(document).on("click", ".btnScrape", function() );
 
-  function scrapeArticles() {
-    event.preventDefault();
-    var section = $("#section")
-      .val()
-      .trim();
-
-    console.log($);
-
-    $.ajax({
-      method: "GET",
-      url: "/scrape/" + section
-    }).then(function(data) {
-      console.log(data);
-    });
-  }
-
   // /articles/state/:saved
-  function handleArticleSave() {
-    // This function is triggered when the user wants to save an article
-    // When we rendered the article initially, we attached a javascript object containing the headline id
-    // to the element using the .data method. Here we retrieve that.
-    event.preventDefault();
-    var articleToSave = $(this)
-      .parents(".card")
-      .data();
-
-    // Remove card from page
-    $(this)
-      .parents(".card")
-      .remove();
-
-    articleToSave.saved = true;
-    // Using a patch method to be semantic since this is an update to an existing record in our collection
-    $.ajax({
-      method: "PUT",
-      url: "/api/articles/" + articleToSave._id,
-      data: articleToSave
-    }).then(function(data) {
-      // If the data was saved successfully
-      if (data.saved) {
-        // Run the initPage function again. This will reload the entire list of articles
-        // initPage();
-        console.log("article saved");
-      }
-    });
-  }
 
   function handleArticleClear() {
     event.preventDefault();
