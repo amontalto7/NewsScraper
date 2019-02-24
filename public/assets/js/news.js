4a2cd6303934a669efd0dff8d34ed7801f1ea4b2
ant4u <anthonyimontalto@gmail.com>
2019-02-23 22:24:28 -0500

    fixed links, styling

--- public/assets/js/news.js
+++ public/assets/js/news.js
@@ -8,40 +8,58 @@ $.getJSON("/articles", function(data) {
   // For each one
   for (var i = 0; i < data.length; i++) {
     // Display the apropos information on the page
-    var articleCard = $("<div>").addClass("card");
-
-    var articleHeader = $("<div>").addClass("card-header  bg-info text-light");
-
-    var articleBody = $("<div>").addClass("card-body");
-    articleBody.attr("data-id", data[i]._id);
-
-    var aHeadline = $("<h5>").addClass("card-title");
-    aHeadline.text(data[i].headline);
-    var aUrl = $("<p>").text(data[i].url);
-    var aSummary = $("<p>").text(data[i].summary);
+    var card = $("<div>").addClass("card");
 
     var saveBtn = $("<a>");
-    saveBtn.addClass("btn btn-success float-right mr-5 save");
+    saveBtn.addClass("btn btn-success mr-5 save");
     saveBtn.attr("data-id", data[i]._id);
     saveBtn.text("SAVE ARTICLE");
     saveBtn.attr("href", "#");
 
-    // var deleteIcon = $("<i>").addClass("material-icons");
-    // deleteIcon.text("delete_forever");
-
     var deleteBtn = $("<a>");
-    deleteBtn.addClass("btn btn-danger float-right align-top delete-this");
+    deleteBtn.addClass("btn btn-danger align-top ml-5 delete-this");
     deleteBtn.attr("data-id", data[i]._id);
     deleteBtn.attr("href", "#");
     // deleteBtn.append(deleteIcon);
     deleteBtn.text("X");
 
-    articleHeader.append(aHeadline, saveBtn, deleteBtn);
-    articleBody.append(aUrl, aSummary);
+    var articleHeader = $(
+      "<div class='card-header bg-info text-light'>"
+    ).append(
+      $("<h5>").append(
+        $("<a />", {
+          text: data[i].headline,
+          rel: "noopener noreferrer",
+          href: "https://www.nytimes.com" + data[i].url,
+          target: "_blank"
+        }),
+        saveBtn,
+        deleteBtn
+      )
+    );
 
-    articleCard.append(articleHeader, articleBody);
+    var articleBody = $("<div>").addClass("card-body");
+    articleBody.attr("data-id", data[i]._id);
 
-    $("#articles").append(articleCard);
+    // var aHeadline = $("<h5>").addClass("card-title");
+    // var aHeadLineURL = $("<a />", {
+    //   text: data[i].headline,
+    //   title: data[i].headline,
+    //   href: "https://www.nytimes.com" + data[i].url,
+    //   target: "_blank"
+    // }).appendTo(aHeadline);
+
+    var aSummary = $("<p>").text(data[i].summary);
+
+    // var deleteIcon = $("<i>").addClass("material-icons");
+    // deleteIcon.text("delete_forever");
+
+    articleHeader.append(deleteBtn);
+    articleBody.append(aSummary);
+
+    card.append(articleHeader, articleBody);
+
+    $("#articles").append(card);
   }
 });
 
