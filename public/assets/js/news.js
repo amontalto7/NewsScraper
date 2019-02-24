5d91b22ea434c26627d032cb56ae881a620a7536
ant4u <anthonyimontalto@gmail.com>
2019-02-23 21:24:04 -0500

    styling

--- public/assets/js/news.js
+++ public/assets/js/news.js
@@ -1,73 +1,77 @@
 $(document).ready(function() {
-  // Grab the articles as a json
-  $.getJSON("/articles", function(data) {
-    // For each one
-    for (var i = 0; i < data.length; i++) {
-      // Display the apropos information on the page
-      var articleCard = $("<div>").addClass("card");
+  var articleContainer = $("#articles");
+  // todo: MAIN LOGIC HERE
+});
 
-      var articleBody = $("<div>").addClass("card-body");
-      articleBody.attr("data-id", data[i]._id);
+// Grab the articles as a json
+$.getJSON("/articles", function(data) {
+  // For each one
+  for (var i = 0; i < data.length; i++) {
+    // Display the apropos information on the page
+    var articleCard = $("<div>").addClass("card");
 
-      var aHeadline = $("<h5>").addClass("card-title");
-      aHeadline.text(data[i].headline);
-      var aUrl = $("<p>").text(data[i].url);
-      var aSummary = $("<p>").text(data[i].summary);
+    var articleHeader = $("<div>").addClass("card-header  bg-info text-light");
 
-      articleBody.append(aHeadline, aUrl, aSummary);
+    var articleBody = $("<div>").addClass("card-body");
+    articleBody.attr("data-id", data[i]._id);
 
-      var notesBtn = $("<a>");
-      notesBtn.addClass("btn btn-outline-primary float-left notes-button");
-      notesBtn.attr("data-id", data[i]._id);
-      notesBtn.text("Notes");
-      notesBtn.attr("href", "#");
+    var aHeadline = $("<h5>").addClass("card-title");
+    aHeadline.text(data[i].headline);
+    var aUrl = $("<p>").text(data[i].url);
+    var aSummary = $("<p>").text(data[i].summary);
 
-      // var deleteIcon = $("<i>").addClass("material-icons");
-      // deleteIcon.text("delete_forever");
+    var saveBtn = $("<a>");
+    saveBtn.addClass("btn btn-success float-right mr-5 save");
+    saveBtn.attr("data-id", data[i]._id);
+    saveBtn.text("SAVE ARTICLE");
+    saveBtn.attr("href", "#");
 
-      var deleteBtn = $("<a>");
-      deleteBtn.addClass("btn btn-danger float-right align-top delete-this");
-      deleteBtn.attr("data-id", data[i]._id);
-      deleteBtn.attr("href", "#");
-      // deleteBtn.append(deleteIcon);
-      deleteBtn.text("X");
+    // var deleteIcon = $("<i>").addClass("material-icons");
+    // deleteIcon.text("delete_forever");
 
-      articleBody.append(notesBtn, deleteBtn);
+    var deleteBtn = $("<a>");
+    deleteBtn.addClass("btn btn-danger float-right align-top delete-this");
+    deleteBtn.attr("data-id", data[i]._id);
+    deleteBtn.attr("href", "#");
+    // deleteBtn.append(deleteIcon);
+    deleteBtn.text("X");
 
-      articleCard.append(articleBody);
+    articleHeader.append(aHeadline, saveBtn, deleteBtn);
+    articleBody.append(aUrl, aSummary);
 
-      $("#articles").append(articleCard);
-    }
+    articleCard.append(articleHeader, articleBody);
+
+    $("#articles").append(articleCard);
+  }
+});
+
+$(document).on("click", ".btnScrape", function() {
+  event.preventDefault();
+  var section = $("#section")
+    .val()
+    .trim();
+
+  console.log($);
+
+  $.ajax({
+    method: "GET",
+    url: "/scrape/" + section
+  }).then(function(data) {
+    console.log(data);
   });
+});
 
-  $(document).on("click", ".btnScrape", function() {
-    event.preventDefault();
-    var section = $("#section")
-      .val()
-      .trim();
+// Click handler for delete buttons
+$(document).on("click", ".delete-this", function() {
+  event.preventDefault();
 
-    console.log($);
+  var id = $(this).attr("data-id");
 
-    $.ajax({
-      method: "GET",
-      url: "/scrape/" + section
-    }).then(function(data) {
-      console.log(data);
-    });
-  });
-
-  // Click handler for delete buttons
-  $(document).on("click", ".delete-this", function() {
-    event.preventDefault();
-
-    var id = $(this).attr("data-id");
-
-    $.ajax({
-      method: "DELETE",
-      url: "/articles/" + id
-    }).then(function(data) {
-      console.log(data);
-      // TODO: Refresh articles on page
-    });
+  $.ajax({
+    method: "DELETE",
+    url: "/articles/" + id
+  }).then(function(data) {
+    console.log(data);
+    // TODO: Refresh articles on page
   });
 });
